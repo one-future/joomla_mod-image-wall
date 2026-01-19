d4cf33d5a13dbac5ef8a1f7bd258ef1396ef9745
theEpsilon <f.berndt99@gmail.com>
2026-01-19 14:47:20 +0100

    update domhelper to use safer jQuery hook

--- mod_image_wall/media/js/domhelper.js
+++ mod_image_wall/media/js/domhelper.js
@@ -1,17 +1,17 @@
-jQuery(document).ready(function() {
-    jQuery("div[class*='imgwall-cell'] svg").each(function() {
-        let aspectratio = jQuery(this).width() / jQuery(this).height();
+jQuery(function($) {
+    $("div[class*='imgwall-cell'] svg").each(function() {
+        let aspectratio = $(this).width() / $(this).height();
 
-        let heightrestr = jQuery(this).closest("figure").attr("data-hr");
+        let heightrestr = $(this).closest("figure").attr("data-hr");
 
-        jQuery(this).css("max-width", heightrestr * aspectratio);
+        $(this).css("max-width", heightrestr * aspectratio);
 
-        jQuery(this).next().css("max-width", heightrestr * aspectratio);
+        $(this).next().css("max-width", heightrestr * aspectratio);
     });
 
-    jQuery("[class*='imgwall-wrapper']").each(function() {
-        let id = jQuery(this).attr('data-mid');
+    $("[class*='imgwall-wrapper']").each(function() {
+        let id = $(this).attr('data-mid');
 
-        jQuery(this).parent().addClass("imgwall-module-" + id);
+        $(this).parent().addClass("imgwall-module-" + id);
     });
 });
